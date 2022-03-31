843867ce6a777260eb39ae2debdea1dbf048edcb
aftabchishti <aftabchishtictn92@gmail.com>
2022-04-01 04:50:02 +0500

    features added

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,13 +1,19 @@
 import React, { useEffect } from "react";
 
 const Weathercard = ({
+  lon,
+  lat,
   temp,
+  feels_like,
+  temp_min,
+  temp_max,
   humidity,
   pressure,
   weathermood,
   name,
   speed,
   country,
+  sunrise,
   sunset,
 }) => {
   const [weatherState, setWeatheState] = React.useState("");
@@ -37,8 +43,18 @@ const Weathercard = ({
 
   // converting the seconds into time
   let sec = sunset;
+  let sec2 = sunrise;
+  let kelvin = 273.15;
   let date = new Date(sec * 1000);
+  let date2 = new Date(sec2 * 1000);
   let timeStr = `${date.getHours()}:${date.getMinutes()}`;
+  let timeStr2 = `${date2.getHours()}:${date2.getMinutes()}`;
+  var temperat = parseFloat(temp - kelvin).toFixed(2);
+  var minimum_temp = parseFloat(temp_min - kelvin).toFixed(2);
+  var maximum_temp = parseFloat(temp_max - kelvin).toFixed(2);
+  var feels_temp = parseFloat(feels_like - kelvin).toFixed(2);
+  
+
   return (
     <>
       <article className="widget">
@@ -48,7 +64,7 @@ const Weathercard = ({
 
         <div className="weatherInfo">
           <div className="temperature">
-            <span>{temp - 273.15}&deg;</span>
+            <span>{temperat}&deg;</span>
           </div>
 
           <div className="description">
@@ -66,11 +82,53 @@ const Weathercard = ({
           <div className="temp-info-minmax">
             <div className="two-sided-section">
               <p>
+                <i className={"wi wi-sunrise"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {timeStr2} AM <br />
+                Sunrise
+              </p>
+            </div>
+            <div className="two-sided-section">
+              <p>
                 <i className={"wi wi-sunset"}></i>
               </p>
               <p className="extra-info-leftside">
                 {timeStr} PM <br />
                 Sunset
+              </p>
+            </div>
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-thermometer-exterior"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {minimum_temp}
+                <i className={"wi wi-degrees"}></i> <br />
+                Minimum Temperature
+              </p>
+            </div>
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-thermometer"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {maximum_temp}
+                <i className={"wi wi-degrees"}></i> <br />
+                Maximum Temperature
+              </p>
+            </div>
+          </div>
+          
+          <div className="weather-extra-info">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-alien"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {feels_temp}
+                <i className={"wi wi-degrees"}></i> <br />
+                Feels Like
               </p>
             </div>
 
@@ -83,9 +141,6 @@ const Weathercard = ({
                 Humidity
               </p>
             </div>
-          </div>
-
-          <div className="weather-extra-info">
             <div className="two-sided-section">
               <p>
                 <i className={"wi wi-rain"}></i>
--- src/components/temp.js
+++ src/components/temp.js
@@ -13,20 +13,27 @@ const Temp = () => {
       let res = await fetch(url);
       let data = await res.json();
 
-      const { temp, humidity, pressure } = data.main;
+      const { lon, lat} = data.coord
+      const { temp, feels_like, temp_min, temp_max, humidity, pressure } = data.main;
       const { main: weathermood } = data.weather[0];
       const { name } = data;
       const { speed } = data.wind;
-      const { country, sunset } = data.sys;
+      const { country, sunrise, sunset } = data.sys;
 
       const myNewWeatherInfo = {
+        lon,
+        lat,
         temp,
+        feels_like,
+        temp_min,
+        temp_max,
         humidity,
         pressure,
         weathermood,
         name,
         speed,
         country,
+        sunrise,
         sunset,
       };
 
